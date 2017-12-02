f694cd94f1cf5af9024bd0334f7dcf15bb001a70
Ewcia100 <epawelska@gmail.com>
2017-12-03 00:34:22 +0100

    Task 9.4 Food2Door-fixed

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/taskFood2Door/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/taskFood2Door/Application.java
@@ -2,8 +2,9 @@ package com.kodilla.good.patterns.challenges.taskFood2Door;
 
 public class Application {
     public static void main(String[] args) {
-        DeliveryRequestRetriever deliveryRequestRetriever = new DeliveryRequestRetriever();
-        DeliveryRequest deliveryRequest = deliveryRequestRetriever.retrieve();
+
+        DeliveryRequest deliveryRequest = new DeliveryRequest
+                ("Extra Food Shop", "apple", 20, "kg");
 
         DeliveryProcessor deliveryProcessor = new DeliveryProcessor();
         deliveryProcessor.order(deliveryRequest);
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/taskFood2Door/GlutenFreeShop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/taskFood2Door/GlutenFreeShop.java
@@ -2,7 +2,7 @@ package com.kodilla.good.patterns.challenges.taskFood2Door;
 
 public class GlutenFreeShop implements FoodSupplier {
     public boolean process(String foodSupplier, String product, int quantity, String unit) {
-        if (unit == "kg" && quantity < 100) {
+        if (!((unit == "kg") && (quantity > 100))) {
             System.out.println("The " + foodSupplier + " can deliver product.");
             return true;
         } else {
